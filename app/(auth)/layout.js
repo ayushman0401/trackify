cfc01f3131f03a096b8a1d51f592fa136ccf2aba
ayushman0401 <ayushmanagarwal789@gmail.com>
2025-05-06 14:31:49 +0530

    initial setup

---
+++ app/(auth)/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const authlayout = ({children}) => {
+  return (
+    <div className='flex justify-center pt-40'>
+      {children}
+    </div>
+  )
+}
+
+export default authlayout
