7abb996bd82c90985cc260984b58a0c6e6c40e8b
ayushman0401 <ayushmanagarwal789@gmail.com>
2025-08-11 22:15:16 +0530

    add settlements-payment-insights feature

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
+import { spendingInsights } from "@/lib/inngest/spending-insights";
 
 export const { GET, POST, PUT } = // All serve handlers have the same arguments:
 serve({
   client: inngest, 
-  functions: [helloWorld,],
+  functions: [
+    paymentReminders,
+    spendingInsights
+  ],
 });
 
